31ab4c1b6c6d34d73f7b8875f8fbfd4bfb68cf4e
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-04-21 08:35:01 +0200

    Search widget

---
+++ script.js
@@ -0,0 +1,8 @@
+const search = document.querySelector('.search');
+const button = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+button.addEventListener('click', () => {
+  search.classList.toggle('active');
+  input.focus();
+});
